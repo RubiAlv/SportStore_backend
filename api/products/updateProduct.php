86191d2a9554bcc2c13172df63c6d4d8c69c83ab
RubiAlv <124765356+RubiAlv@users.noreply.github.com>
2023-05-26 12:51:00 -0600

    actualizacion del backend

---
+++ api/products/updateProduct.php
@@ -0,0 +1,28 @@
+<?php
+require "../config/database.php";
+
+class query extends Conexion {
+    public function config() {
+        $id = $_GET['id'];
+        $title = $_GET['title'];
+        $categoryID = $_GET['categoryID'];
+        $description = $_GET['description'];
+        $sex = $_GET['categorySex'];
+        $precio = $_GET['precio'];
+        $image = $_GET['imageUrl'];
+        $talla = $_GET['talla'];
+        $existencia = $_GET['existencia'];
+        $tags = strtolower($description);
+        try {
+            $conexion = parent::conectar();
+            $query = new MongoDB\Driver\BulkWrite;
+            $query->update(['_id' => new MongoDB\BSON\ObjectID($id)], ['$set' => ['title'=>$title, 'description'=>$description, 'categorySex'=>$sex, 'precio'=>floatval($precio), 'categoryID'=>$categoryID, 'imageUrl'=>$image, 'talla'=>$talla, 'tags' => $tags, 'existencia' => floatval($existencia)]]);
+            $conexion->executeBulkWrite($this->database_name.$this->col_products, $query);
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+}
+$obj = new query();
+echo json_encode($obj->config());
+?>
